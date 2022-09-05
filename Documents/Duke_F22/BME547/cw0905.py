dd18376f73aa1b05778879cdb4dd24e687e08d34
Britney Chu <britneychu80@gmail.com>
2022-09-05 12:31:17 -0400

    wrote basic framework for user input

---
+++ Documents/Duke_F22/BME547/cw0905.py
@@ -0,0 +1,21 @@
+# def increment_by_five(x):
+#     a = x + 5
+#     if a>= 18:
+#         adult = True
+#     else:
+#         adult = False
+#     return a, adult
+
+# answer, is_adult = increment_by_five(11)
+# print("The answer is {}".format(answer))
+# print("True or false, the person is an adult: {}".format(is_adult))
+
+def interface():
+    print("Blood Calculator")
+    print("Options: ")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    if choice=='9':
+        return
+   
+interface()
