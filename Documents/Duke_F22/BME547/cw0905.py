7a54331baa80cc531a1934d2690eadb56d3fd90e
Britney Chu <britneychu80@gmail.com>
2022-09-05 13:16:41 -0400

    fixed bug in user input : would not exit while loop

--- Documents/Duke_F22/BME547/cw0905.py
+++ Documents/Duke_F22/BME547/cw0905.py
@@ -13,15 +13,19 @@
 def interface():
     print("Blood Calculator")
     print("Options: ")
+    print("1 - analyze HDL")
     print("9 - Quit")
     
     keep_running = True
     while keep_running:
-        choice = input("Enter your choice: ")
-        if choice=='9':
+        choice = int(input("Enter your choice: "))
+        if choice == 9:
+
             return
+        elif choice == 1:
+            HDL_driver()
 def user_input():
-    user_in = input("Enter HDL: ")
+    user_in = input("Enter HDL value: ")
     return int(user_in)
 def check_HDL(HDLNum):
     if HDLNum >= 60:
@@ -30,5 +34,13 @@ def check_HDL(HDLNum):
         return "Borderline Low"
     else:
         return "Low"
-# take input from keyboard as a number then return that number 
+#driver function: calls other funtions and moves variables in between them
+def HDL_driver():
+    hdl_value = user_input()
+    answer = check_HDL(hdl_value)
+    output_HDL_result(hdl_value, answer)
+
+def output_HDL_result(HDL_value, charac):
+    print("The results for an HDL value of {} is {}".format(HDL_value, charac))
+
 interface()
