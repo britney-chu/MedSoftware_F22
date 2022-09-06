ad9f0c3a38232aad0e8b7d6f67115e9ee7ed9324
Britney Chu <britneychu80@gmail.com>
2022-09-05 20:20:39 -0400

    fixing git hub repository

--- Documents/Duke_F22/BME547/cw0905_2.py
+++ Documents/Duke_F22/BME547/cw0905_2.py
@@ -14,6 +14,7 @@ def interface():
     print("Blood Calculator")
     print("Options: ")
     print("1 - analyze HDL")
+    print("2 - analyze LDL")
     print("9 - Quit")
     
     keep_running = True
@@ -23,6 +24,8 @@ def interface():
             return
         elif choice == "1":
             HDL_driver()
+        elif choice == "2":
+            LDL_driver()
 def user_input():
     user_in = input("Enter HDL value: ")
     return int(user_in)
@@ -38,8 +41,23 @@ def HDL_driver():
     hdl_value = user_input()
     answer = check_HDL(hdl_value)
     output_HDL_result(hdl_value, answer)
+def LDL_driver():
+    ldl_value = user_input()
+    answer = check_LDL(ldl_value)
+    output_LDL_result(ldl_value, answer)
+def check_LDL(LDLNum):
+    if LDLNum < 130:
+        return "LDL is normal"
+    elif 130 <= LDLNum <160:
+        return "LDL is borderline high"
+    elif 160 <= LDLNum <190:
+        return "LDL is high"
+    else:
+        return "LDL is very high"
 
 def output_HDL_result(HDL_value, charac):
     print("The results for an HDL value of {} is {}".format(HDL_value, charac))
+def output_LDL_result(LDL_value, charac):
+    print("The results for an LDL value of {} is {}".format(LDL_value, charac))
 
 interface()
